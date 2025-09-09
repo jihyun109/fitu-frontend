75e5fca7177bf485b96a0649c27f79dd75cc91ac
CHO YONGSU <choyongsu2001@naver.com>
2025-09-10 01:56:05 +0900

    feat: 회원가입 추가정보 입력란 개발 #6

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './pages/login/Login';
 import AdminLogin from './pages/adminLogin/adminLogin';
 import { Route, Routes, useLocation } from "react-router-dom";
+import MoreInfo from './pages/login/moreInfo/MoreInfo';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/adminlogin' element={<AdminLogin/>}/>
+        <Route path='/signup' element={<MoreInfo/>}/>
       </Routes>
     </div>
   );
