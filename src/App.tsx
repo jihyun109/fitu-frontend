9ceabcd1716722880231b09cf3017e58d1c9fff2
jumin1 <wnals1018@gmail.com>
2025-09-10 11:20:56 +0900

    페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,14 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/adminrequest" element={<AdminRequest />} />
         <Route path="/adminpost" element={<AdminPost />} />
         <Route path="/adminmember" element={<AdminMember />} />
         <Route path='/' element={<Login/>}/>
         <Route path='/adminlogin' element={<AdminLogin/>}/>
+        <Route path='/signup' element={<MoreInfo/>}/>
         <Route path='/mypage' element={<MyPage/>}/>
       </Routes>
     </div>
