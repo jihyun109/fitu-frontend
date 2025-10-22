3c658d0dda7ae1c23466c0cb8ac46caf408116b9
CHO YONGSU <choyongsu2001@naver.com>
2025-10-22 22:06:01 +0900

    feat:토큰 재발급 기능추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,13 +1,19 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_SERVER_URL
+
 const axiosInstance = axios.create({
-  baseURL: `${BASE_URL}`,
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, 
 });
 
+const refreshClient = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, 
+});
 axiosInstance.interceptors.request.use(
   (config) => {
     const accessToken = sessionStorage.getItem("Authorization");
@@ -18,15 +24,34 @@ axiosInstance.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
-// 아직 리프레쉬 토큰 없음
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     if (error.response?.status === 401) {
-//       const refreshToken = sessionStorage.getItem("RefreshToken");
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshRes = await refreshClient.get("/auth/reissue");
+        const newAccessToken = refreshRes.data?.accessToken;
+
+        if (newAccessToken) {
+          sessionStorage.setItem("Authorization", newAccessToken);
+          axiosInstance.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${newAccessToken}`;
+          originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+          return axiosInstance(originalRequest);
+        }
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        console.error("재발급 실패",refreshError);
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
